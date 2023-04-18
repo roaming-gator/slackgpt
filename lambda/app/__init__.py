6ad49c43a2353c64d1d1a86db1e07397f1f8c8f8
Paul Nickerson <pvnick>
2023-04-18 17:41:49 -0400

    add aws_xray_sdk

--- lambda/app/__init__.py
+++ lambda/app/__init__.py
@@ -6,7 +6,7 @@ patch_all()
 
 # pip install custom package to /tmp/ and add to path
 subprocess.call(
-    "pip install openai requests -t /tmp/ --no-cache-dir".split(),
+    "pip install openai requests aws_xray_sdk -t /tmp/ --no-cache-dir".split(),
     stdout=subprocess.DEVNULL,
     stderr=subprocess.DEVNULL,
 )
