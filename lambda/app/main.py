0f185f7b2650526dc429503af722dc9dbf5053e7
Paul Nickerson <pvnick>
2023-04-18 16:55:38 -0400

    go

--- lambda/app/main.py
+++ lambda/app/main.py
@@ -6,6 +6,7 @@ def lambda_handler(event, context):
     logging.info(f"Received event:\n{event}\nWith context:\n{context}")
     headers = event["headers"]
     body = event["body"]
+    logging.info(f"headers: {headers}")
     result = slack.process_request(headers, body)
     return {
         "statusCode": result.status_code,
