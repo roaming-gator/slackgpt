d929bc999c8587e90a2620b65ddc0a590da3b9b3
Paul Nickerson <pvnick>
2023-04-24 15:52:37 -0400

    try this

--- terraform/package.tf
+++ terraform/package.tf
@@ -7,25 +7,28 @@ resource "null_resource" "python_scripts_setup" {
   depends_on = [
     local_file.kick
   ]
-  # trigger whenever any file changes in the lambda directory
   triggers = {
-    redeployment = local.package_source_hash
+    # always trigger the dependency installation on each run.
+    # this is needed as a workaround for lack of file persistence on terraform cloud.
+    # todo: figure out a way to only do this when the source code changes
+    redeployment = timestamp()
   }
   provisioner "local-exec" {
     command     = <<EOT
       set -e
       FOLDER_PATH="${path.module}/../lambda/"
       TEMP_DIR="${local.python_package_dir}"
+      mkdir -p "$TEMP_DIR"
       echo "Temporary directory created: $TEMP_DIR"
       
       # Copy Python scripts to the temporary directory
-      cp -R $FOLDER_PATH/* $TEMP_DIR/
+      cp -R "$FOLDER_PATH/*" "$TEMP_DIR/"
       echo "Python scripts copied to $TEMP_DIR"
 
       # Check if 'requirements.txt' exists in the folder
       if [ -f "$TEMP_DIR/requirements.txt" ]; then
         # Install package dependencies to the temporary directory
-        pip install -r $TEMP_DIR/requirements.txt --target $TEMP_DIR
+        pip install -r "$TEMP_DIR/requirements.txt" --target "$TEMP_DIR"
         echo "Dependencies installed in $TEMP_DIR"
       else
         echo "No requirements.txt file found, skipping dependencies installation"
@@ -37,7 +40,8 @@ resource "null_resource" "python_scripts_setup" {
 
 data "archive_file" "python_lambda_package" {
   depends_on = [
-    null_resource.python_scripts_setup
+    null_resource.python_scripts_setup,
+    local_file.kick
   ]
   excludes = [
     "__pycache__",
