d7b052cf590fb58241b7a640ee03cc1aab89c95c
Paul Nickerson <pvnick>
2023-04-24 12:24:22 -0400

    fix manifest output format

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,25 +3,34 @@ output "api_gateway_url" {
 }
 
 output "slack_app_manifest" {
-  value = <<-EOF
-    display_information:
-      name: ${var.slack_bot_display_name}
-    features:
-      bot_user:
-        display_name: ${var.slack_bot_display_name}
-        always_online: true
-    oauth_config:
-      scopes:
-        bot:
-          - app_mentions:read
-          - chat:write
-    settings:
-      event_subscriptions:
-        request_url: "${aws_api_gateway_stage.this.invoke_url}/${var.api_gateway_resource_path}"
-        bot_events:
-          - app_mention
-      org_deploy_enabled: false
-      socket_mode_enabled: false
-      token_rotation_enabled: false
-    EOF
+  value = jsonencode({
+    display_information = {
+      name = var.slack_bot_display_name
+    },
+    oauth_config = {
+      scopes = {
+        bot = [
+          "app_mentions:read",
+          "chat:write"
+        ]
+      }
+    },
+    features = {
+      bot_user = {
+        display_name  = var.slack_bot_display_name
+        always_online = true
+      }
+    },
+    settings = {
+      event_subscriptions = {
+        request_url = "${aws_api_gateway_stage.this.invoke_url}/${var.api_gateway_resource_path}"
+        bot_events = [
+          "app_mention"
+        ]
+      },
+      org_deploy_enabled     = false,
+      socket_mode_enabled    = false,
+      token_rotation_enabled = false
+    }
+  })
 }
